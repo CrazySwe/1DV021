df3466999aca1e8ef9e2dfea897c6ad8a15dc3d5
Kevin C <crazy_swe_@hotmail.com>
2018-09-06 14:16:27 +0200

    Fixed determinator.js and tellType function

--- data-type-determinator/src/determinator.js
+++ data-type-determinator/src/determinator.js
@@ -19,7 +19,32 @@ function tellType (data) {
   let result = 'You sent me '
 
   // TODO: Write your code here.
-
+  switch (typeof data) {
+    case 'string':
+      result += 'a string.'
+      break
+    case 'boolean':
+      result += 'a boolean.'
+      break
+    case 'number':
+      result += 'a number.'
+      break
+    case 'function':
+      result += 'a function.'
+      break
+    default:
+    // Objects are many things. Also undefined goes to default.
+      if (data === null) {
+      // Have to check manually for null since else it returns object for legacy reasons. Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null
+        result += 'a null value.'
+      } else if (Array.isArray(data)) {
+        result += 'an array.'
+      } else if (typeof data === 'object') {
+        result += 'an object.'
+      } else {
+        result += 'an undefined value.'
+      }
+  }
   return result
 }
 
