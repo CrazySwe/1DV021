005acaebec527ee9def3d391b7cdb38e28348f3a
Kevin C <crazy_swe_@hotmail.com>
2018-09-06 14:25:57 +0200

    Fixed app.js calls to tellType function

--- data-type-determinator/app.js
+++ data-type-determinator/app.js
@@ -10,5 +10,12 @@
 
 const determinator = require('./src/determinator')
 
-console.log(determinator.tellType('Hasta la vista, baby.'))
+console.log(determinator.tellType('Hasta la vista, baby.')) // outputs 'You sent me a string.'
 // TODO: Write your code here.
+console.log(determinator.tellType(42)) // outputs 'You sent me a number.'
+console.log(determinator.tellType(null)) // outputs 'You sent me a null value.'
+console.log(determinator.tellType(true)) // outputs 'You sent me a boolean.'
+console.log(determinator.tellType({})) // outputs 'You sent me an object.'
+console.log(determinator.tellType([1, 2, 3])) // outputs 'You sent me an array.'
+console.log(determinator.tellType(function () {})) // outputs 'You sent me a function.'
+console.log(determinator.tellType(undefined)) // outputs 'You sent me an undefined value.'
