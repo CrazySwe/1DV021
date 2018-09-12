533a78d0b69488547cfd5fe8a938b959de568122
Kevin C <crazy_swe_@hotmail.com>
2018-09-12 15:48:45 +0200

    second attempt at not making a mess of add function

--- abacus/src/abacus.js
+++ abacus/src/abacus.js
@@ -16,27 +16,40 @@
  */
 function add (varArgs) {
   // Utilize the 'arguments' object here, not the 'varArgs' parameter.
-  // 1. get the arguments, 2.sort them after parsing need and 3.return the sum of all.
-  // console.log('\nNr of arguments: ' + arguments.length + '\n')
-  if (arguments.length === 0) {
-    return 0
+  // Second try using spread operator.
+  let result = [...arguments].reduce((acc, val) => {
+    // concat val with acc to flatten the array
+    return acc.concat(val)
+  }, [])
+  // Check if any value is NotANumber
+  if (result.some(val => isNaN(val))) {
+    throw new TypeError(`At least one of the arguments can't be parsed as a number.`)
   }
-  let args = Array.prototype.slice.call(arguments).reduce((acc, val) => {
-    if (typeof val === 'number') {
-      return acc + val
-    } else if (typeof val === 'string') {
-      let res = parseFloat(val.replace(',', '.'))
-      if (isNaN(res)) { throw new TypeError(`At least one of the arguments can't be parsed as a number.`) }
-      return acc + parseFloat(val.replace(',', '.'))
-    } else if (Array.isArray(val)) {
-      return acc + val.reduce((acc1, val1) => acc1 + val1)
-    } else if (typeof val === 'boolean') {
-      throw new TypeError(`At least one of the arguments can't be parsed as a number.`)
-    }
-  }, 0)
-  // args.reduce((acc, val, index) => { return acc + val })
-  // TODO: Write your code here.
-  return args
+  // Now all values are checked, start adding them and/or parsing strings
+
+  // debugger
+  // console.log(result)
+  return result
+
+  // First try - Very messy
+  // if (arguments.length === 0) {
+  //   return 0
+  // }
+  // let args = Array.prototype.slice.call(arguments).reduce((acc, val) => {
+  //   if (typeof val === 'number') {
+  //     return acc + val
+  //   } else if (typeof val === 'string') {
+  //     let res = parseFloat(val.replace(',', '.'))
+  //     if (isNaN(res)) { throw new TypeError(`At least one of the arguments can't be parsed as a number.`) }
+  //     return acc + parseFloat(val.replace(',', '.'))
+  //   } else if (Array.isArray(val)) {
+  //     return acc + val.reduce((acc1, val1) => acc1 + val1)
+  //   } else if (typeof val === 'boolean') {
+  //     throw new TypeError(`At least one of the arguments can't be parsed as a number.`)
+  //   }
+  // }, 0)
+  // // TODO: Write your code here.
+  // return args
 }
 
 exports.add = add
