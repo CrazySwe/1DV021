dae311a69b97deca5572863c1e29d20464d791b7
Kevin C <crazy_swe_@hotmail.com>
2018-09-18 01:45:50 +0200

    Added Errorhandling in function and getMax function
    Added a simple error handling function to validate data input in.
    getMax function added to return highest value in array of numbers.

--- src/statistics.js
+++ src/statistics.js
@@ -18,14 +18,55 @@
  * @returns {{maximum: number, mean: number, median: number, minimum: number, mode: number[], range: number, standardDeviation: number}}
  */
 function descriptiveStatistics (numbers) {
-  // TODO: Write your code here.
+  validateData(numbers)
+  // create my return here by calling my other functions
+  // return { maximum: maximum(), mean: mean(), median: median(), minimum: minimum(), mode: mode(), range: range(), standardDeviation: standardDeviation() }
 }
 
-// TODO: Write your code here.
+/**
+ * Returns the maximum value
+ *
+ * @param {number[]} numbers The set of numbers to get maximum value from.
+ * @throws {TypeError} The passed argument is not an array.
+ * @throws {Error} The passed array contains no elements.
+ * @throws {TypeError} The passed array contains not just numbers.
+ * @returns {number} The maximum value to return.
+ */
+function getMax (numbers) {
+  validateData(numbers)
+  return Math.max(...numbers)
+}
+// function getMean
+// function getMedian
+// function getMinimum
+// function getMode
+// function getRange
+// function getStandardDeviation
+
+// Error Handling
+/**
+ * Validates the input data is Array, contains numbers else returns false.
+ *
+ * @param {number[]} numbers Data to be validated
+ * @throws {TypeError} The passed argument is not an array.
+ * @throws {Error} The passed array contains no elements.
+ * @throws {TypeError} The passed array contains not just numbers.
+ * @returns {false} Returns 0 if passed
+ */
+function validateData (numbers) {
+  if (!Array.isArray(numbers)) {
+    throw new TypeError('The passed argument is not an array.')
+  } else if (!numbers.every(val => typeof val === 'number')) {
+    throw new TypeError('The passed array contains not just numbers.')
+  } else if (!numbers.length) {
+    throw new Error('The passed array contains no elements.')
+  }
+  return 0
+}
 
 // Exports
 exports.descriptiveStatistics = descriptiveStatistics
-exports.maximum = undefined
+exports.maximum = getMax
 exports.mean = undefined
 exports.median = undefined
 exports.minimum = undefined
