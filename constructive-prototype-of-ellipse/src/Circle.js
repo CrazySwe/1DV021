b102b4ab61430ee9a843ff6f92dee20b04544358
Kevin C <crazy_swe_@hotmail.com>
2018-09-28 00:41:52 +0200

    Beginning of circle.js

--- constructive-prototype-of-ellipse/src/Circle.js
+++ constructive-prototype-of-ellipse/src/Circle.js
@@ -6,3 +6,26 @@
  */
 
 'use strict'
+
+/**
+ *  Creates an object representing a circle
+ *
+ * @param {number} radius Radius of the circle
+ */
+function Circle (radius) {
+  this.radius = radius
+}
+
+Circle.prototype.getArea = function () {
+
+}
+
+Circle.prototype.getCircumference = function () {
+
+}
+
+Circle.prototype.toString = function () {
+
+}
+// Exports
+module.exports = Circle
