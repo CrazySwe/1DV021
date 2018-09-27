7b8796beb61310da710bf45b233cf41f9efd53c0
Kevin C <crazy_swe_@hotmail.com>
2018-09-28 01:11:54 +0200

    Adding inheriting Ellipse to circle.js
    inherits properties

--- constructive-prototype-of-ellipse/src/Circle.js
+++ constructive-prototype-of-ellipse/src/Circle.js
@@ -7,25 +7,33 @@
 
 'use strict'
 
+// Reuse code from Ellipse
+const Ellipse = require('./Ellipse.js')
+
 /**
  *  Creates an object representing a circle
  *
  * @param {number} radius Radius of the circle
  */
 function Circle (radius) {
-  this.radius = radius
-}
+  Ellipse.call(this, radius, radius)
 
-Circle.prototype.getArea = function () {
-
-}
-
-Circle.prototype.getCircumference = function () {
-
+  // custom getters/setters : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#Custom_Setters_and_Getters
+  Object.defineProperty(this, 'radius', {
+    configurable: true,
+    get: function () { return this.a },
+    set: function (value) { this.a = this.b = value }
+  })
 }
 
 Circle.prototype.toString = function () {
-
+  return `radius: ${this.radius}, area: ${this.getArea().toFixed(1)}, circumference: ${this.getCircumference().toFixed(1)}`
 }
+
+// Debugging
+let x = new Circle(3)
+x.radius = 25
+console.log(x.radius)
+
 // Exports
 module.exports = Circle
