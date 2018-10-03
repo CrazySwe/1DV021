3684c0f86777c23d6fe92ad1c4f9dfec2e7b1f67
Kevin C <crazy_swe_@hotmail.com>
2018-10-03 03:34:07 +0200

    Adding deck.js file

---
+++ src/deck.js
@@ -0,0 +1,24 @@
+/**
+ * Module for a deck of cards
+ *
+ * @author Kevin Cederholm
+ * @version 1.0
+ */
+
+'use strict'
+
+const Card = require('./card.js')
+
+class Deck {
+  constructor () {
+    // create and store cards
+  }
+  shuffle () {
+    // shuffle the cards
+  }
+  toString () {
+    // print the deck to console
+  }
+}
+
+module.exports = Deck
