7667718cda21c25b15784ccf707aa51c77342165
Kevin C <crazy_swe_@hotmail.com>
2018-10-03 03:55:23 +0200

    Updating documentation in card.js

--- src/card.js
+++ src/card.js
@@ -8,23 +8,38 @@
 'use strict'
 
 const protoCard = {
+  /**
+   * Renders the card to the console
+   */
   render () {
     console.log(this.toString())
   },
+  /**
+   * @returns {string} Representing the card
+   */
   toString () {
     return this.value + this.suit
   }
 }
 
+/**
+ * Returns an object representing a card
+ *
+ * @param {string|number} val - The value of the card
+ * @param {string} suit - The suit of the card
+ * @returns {object} obj - object representing a card
+ * @returns {number|string} obj.value - The value of the card
+ * @returns {string} obj.suit - The suit of the card
+ */
 let createCard = function (val, suit) {
   return Object.create(protoCard, {
-    'value': {
+    value: {
       value: val,
       writable: true,
       enumerable: true,
       configurable: true
     },
-    'suit': {
+    suit: {
       value: suit,
       writable: true,
       enumerable: true,
@@ -33,4 +48,5 @@ let createCard = function (val, suit) {
   })
 }
 
+// Exports
 exports.createCard = createCard
