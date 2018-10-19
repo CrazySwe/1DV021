50cb82e8e5bb2253ae8ac69347348c4540dd1d40
Kevin C <crazy_swe_@hotmail.com>
2018-10-19 17:22:04 +0200

    Adding Player.js class

---
+++ src/player.js
@@ -0,0 +1,21 @@
+/**
+ * player.js
+ *
+ * @author Kevin Cederholm
+ * @version 1.0
+ */
+'use strict'
+
+/**
+ * Object representing a player or a dealer
+ *
+ * @class Player
+ */
+class Player {
+  constructor () {
+    this.hand = []
+    // this.done = false
+  }
+}
+
+module.exports = Player
