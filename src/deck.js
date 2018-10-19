d431e1bd5c16fa4fca42d8858edb389e6dbf6ea7
Kevin C <crazy_swe_@hotmail.com>
2018-10-19 14:53:55 +0200

    Completing constructor and drawCard, Adding createNewDeck and JSDOC

--- src/deck.js
+++ src/deck.js
@@ -9,18 +9,41 @@
 
 const Card = require('./card.js')
 
+/**
+ * Represents a deck of 52 cards.
+ *
+ * @class Deck
+ */
 class Deck {
   constructor () {
-    this.cards = []
+    this.cards = this.createNewDeck()
+    this.shuffle()
+    this.usedCards = []
+  }
+
+  /**
+   * Returns an array with Card objects that represents a deck of 52 cards
+   *
+   * @returns {Array} of cards
+   * @memberof Deck
+   */
+  createNewDeck () {
     let suits = ['♥', '♠', '♣', '♦']
     let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-
+    let newDeck = []
     suits.forEach(suit => {
       values.forEach(val => {
-        this.cards.push(Card.createCard(val, suit))
+        newDeck.push(Card.createCard(val, suit))
       })
     })
+    return newDeck
   }
+
+  /**
+   * Method to shuffle the deck of cards
+   *
+   * @memberof Deck
+   */
   shuffle () {
     // Fisher-Yates shuffle
     let index = 0
@@ -32,17 +55,31 @@ class Deck {
       this.cards[index] = tmp
     }
   }
+
+  /**
+   * Draws a card from this.cards if atleast 2 cards left,
+   * else it will concat the used cards and reshuffle before drawing a card.
+   *
+   * @returns {Object} Returns a Card object
+   * @memberof Deck
+   */
   drawCard () {
     if (this.cards.length > 1) {
       return this.cards.pop()
     } else {
-      // refill this.cards and reshuffle
-      // get cards and put them back into this.cards?
-      // shuffle
-      // Then return a card drawn from this.cards
+      this.cards = this.cards.concat(this.usedCards)
+      this.usedCards = []
+      this.shuffle()
       return this.cards.pop()
     }
   }
+
+  /**
+   * Method to return a string that represent the deck of cards
+   *
+   * @returns {string} String representing the deck of cards
+   * @memberof Deck
+   */
   toString () {
     let str = ''
     this.cards.forEach(card => { str += card.toString() + ' ' })
